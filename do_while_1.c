5c19444f51e34b88c10ecd176b70ceb1beeb6347
watashijoy777-hunt <watashijoy777@gmail.com>
2026-03-09 19:30:24 +0530

    Add files via upload

---
+++ do_while_1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void main ()
+{
+
+        int num=1;
+
+     do
+     {
+        printf("%d\n",num);
+        num++;
+
+     }while(num<=10);
+     
+}
---
+++ do_while_2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main ()
+
+{
+        int num=2,i=1;
+
+        do
+        {
+            printf("%d * %d = %d",num,i,num*i);
+            i++;
+        }while(i<=10);
+
+}
